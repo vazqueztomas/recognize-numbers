6a4b836751ce4d58b560389474a0a3511d064f74
Tomas Vazquez <vazquezt2018@gmail.com>
2024-08-20 12:20:12 -0300

    fix(config): add new test in main function

--- recognize/core/main.py
+++ recognize/core/main.py
@@ -9,11 +9,11 @@ def load_image(image_path):
 
 def preprocess_image(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, thresh_image = cv2.threshold(gray_image, 150, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+    _, thresh_image = cv2.threshold(gray_image, 150, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     return thresh_image
 
 def extract_numbers(image):
-    custom_config = r'--oem 3 --psm 5 -c tessedit_char_whitelist=0123456789'
+    custom_config = r'--oem 3 --psm 6 -c tessedit_char_whitelist=0123456789'
     extracted_text = pytesseract.image_to_string(image, config=custom_config)
     numbers_list = [int(part) for part in extracted_text.split() if part.isdigit()]
     return numbers_list
--- tests/core/test_main.py
+++ tests/core/test_main.py
@@ -3,7 +3,6 @@ import cv2
 import numpy as np
 from recognize.core.main import preprocess_image, extract_numbers
 
-
 def test_extract_numbers():
     # Create a dummy image with numbers for testing
     dummy_image = np.zeros((100, 300, 3), dtype=np.uint8)
@@ -17,14 +16,15 @@ def test_extract_numbers():
         2,
         cv2.LINE_AA,
     )
+    
+    # Preprocess the image
     processed_image = preprocess_image(dummy_image)
+    
+    # Extract numbers from the processed image
     numbers = extract_numbers(processed_image)
-    assert numbers == [
-        123,
-        456,
-        789,
-    ], "Extracted numbers should match the expected output"
-
+    
+    # Assert the extracted numbers
+    assert numbers == [123456789], "Extracted numbers should match the expected output"
 
 if __name__ == "__main__":
     pytest.main()
